faf38824c4250e0dfe52b2a940d6c41d99311e96
Juan Gomez <juancarlos.gm931@gmail.com>
2023-04-29 14:07:17 -0500

    client/src/components Home & Pagination
    Se adptan correctamente

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,48 +1,55 @@
 import React, { useState } from 'react';
 import styles from './Pagination.module.css';
 
-function Pagination(props) {
-	const { recipes, pageSize } = props;
+export default function Pagination({ recipes, pageSize, children }) {
 	const [currentPage, setCurrentPage] = useState(1);
 	const pageCount = Math.ceil(recipes.length / pageSize);
+	const indexOfLastRecipe = currentPage * pageSize;
+	const indexOfFirstRecipe = indexOfLastRecipe - pageSize;
 
 	function handlePageClick(page) {
 		setCurrentPage(page);
-		props.onPageChange(page);
 	}
 
 	return (
-		<div className={styles.pageBtnContainer}>
-			{currentPage > 1 && (
-				<button
-					onClick={() => handlePageClick(currentPage - 1)}
-					className={styles.btnPage}>
-					{'<'}
-				</button>
-			)}
+		<>
+			<div className={styles.pageBtnContainer}>
+				{currentPage > 1 && (
+					<button
+						onClick={() => handlePageClick(currentPage - 1)}
+						className={styles.btnPage}>
+						{'<'}
+					</button>
+				)}
 
-			{Array.from({ length: pageCount }).map((_, index) => (
-				<button
-					key={index}
-					onClick={() => handlePageClick(index + 1)}
-					className={`${
-						currentPage === index + 1
-							? styles.btnPageActive
-							: styles.btnPage
-					}`}>
-					{index + 1}
-				</button>
-			))}
+				{Array.from({ length: pageCount }).map((_, index) => (
+					<button
+						key={index}
+						onClick={() => handlePageClick(index + 1)}
+						className={`${
+							currentPage === index + 1
+								? styles.btnPageActive
+								: styles.btnPage
+						}`}>
+						{index + 1}
+					</button>
+				))}
 
-			{currentPage < pageCount && (
-				<button
-					onClick={() => handlePageClick(currentPage + 1)}
-					className={styles.btnPage}>
-					{'>'}
-				</button>
-			)}
-		</div>
+				{currentPage < pageCount && (
+					<button
+						onClick={() => handlePageClick(currentPage + 1)}
+						className={styles.btnPage}>
+						{' '}
+						{'>'}{' '}
+					</button>
+				)}
+			</div>
+			{children({
+				currentRecipe: recipes.slice(
+					indexOfFirstRecipe,
+					indexOfLastRecipe,
+				),
+			})}
+		</>
 	);
 }
-
-export default Pagination;
