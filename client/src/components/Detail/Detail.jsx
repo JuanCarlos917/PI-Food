bd68c136a5fc6bedf83e54e9a0d02d99dfcbed91
Juan Gomez <juancarlos.gm931@gmail.com>
2023-05-04 14:57:28 -0500

    client/src/component/Detail GET
    Se da orden semantico al codigo

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,88 +1,90 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Detail.module.css';
 import loading from '../../assets/img.png';
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getRecipeByDetail } from '../../redux/actions';
 
 export default function Detail() {
-	const recipeDet = useSelector((state) => state.newRecipes);
+	const { title, diets, summary, healthScore, image, steps } =
+		useSelector((state) => state.newRecipes);
 	const [isLoading, setIsLoading] = useState(true);
 	const dispatch = useDispatch();
-const { id } = useParams();
+	const { id: recipeId } = useParams();
 
 	useEffect(() => {
-		dispatch(getRecipeByDetail(id));
+		dispatch(getRecipeByDetail(recipeId));
 		setIsLoading(false);
-	}, [dispatch,id]);
+	}, [dispatch, recipeId]);
 
-	function removerCaracteres(str) {
-		if (str === null || str === '') return false;
-		else str = str.toString();
-
-		return str.replace(/(<([^>]+)>)/gi, '');
-	}
+	const removerCaracteres = (str) => {
+		if (!str) return false;
+		return str.toString().replace(/(<([^>]+)>)/gi, '');
+	};
 
 	return (
-		<div className={styles.detailSection}>
+		<main>
 			{isLoading ? (
 				<div className={styles.loading}>
 					<img src={loading} alt='Loading...' />
 				</div>
 			) : (
-				<div>
-					<div className={styles.overlay} />
-					<div className={styles.title}>
-						<h2>{recipeDet.title}</h2>
-					</div>
+				<div className={styles.recipeContainer}>
+					<header className={styles.recipeHeader}>
+						<h1>{title}</h1>
+					</header>
 
-					<div className={styles.recipeDetail}>
-						<div>
-							<div className={styles.infoContainer}>
-								<div className={styles.diets}>
-									<p id={styles.diets}>
-										{recipeDet.diets &&
-											recipeDet.diets.map((diet, ind) => (
-												<span
-													key={ind}
-													className={styles.span}>
-													{diet.name}
-												</span>
-											))}
-									</p>
-								</div>
-								<p>ID: {recipeDet.id}</p>
-								<p>
-									{recipeDet.summary &&
-										removerCaracteres(recipeDet.summary)}
-								</p>
-								<p>Health Score: {recipeDet.healthScore}</p>
+					<section className={styles.recipeDetails}>
+						<div className={styles.recipeInfo}>
+							<div className={styles.recipeDiets}>
+								<ul>
+									{diets ? (
+										diets.map((diet, index) => (
+											<li key={index}>{diet.name}</li>
+										))
+									) : (
+										<li>No diets found</li>
+									)}
+								</ul>
 							</div>
-							<div className={styles.img}>
-								<img src={recipeDet.image} alt='' />
+
+							<div className={styles.recipeSummary}>
+								<h2>Summary</h2>
+								<p>{summary && removerCaracteres(summary)}</p>
+							</div>
+
+							<div className={styles.recipeHealthScore}>
+								<h2>Health Score</h2>
+								<p>{healthScore}</p>
 							</div>
 						</div>
+
+						<div className={styles.recipeImage}>
+							<img src={image} alt='' />
+						</div>
+					</section>
+
+					<section className={styles.recipeSteps}>
 						<h2>Steps:</h2>
-						<div className={styles.listSteps}>
+						<div className={styles.recipeStepList}>
 							<ul>
-								{recipeDet.steps &&
-									recipeDet.steps.map((x, index) => (
+								{steps &&
+									steps.map((e, index) => (
 										<li key={index}>
-											{x.number}: {x.step}
+											{e.number}: {e.step}
 										</li>
 									))}
 							</ul>
 						</div>
-						<Link
-							to={'/home'}
-							className={styles.button}
-							style={{ textDecoration: 'none' }}>
-							Back
+					</section>
+
+					<section className={styles.recipeBackToHome}>
+						<Link to={'/home'} className={styles.backButton}>
+							Back to home
 						</Link>
-					</div>
+					</section>
 				</div>
 			)}
-		</div>
+		</main>
 	);
 }
